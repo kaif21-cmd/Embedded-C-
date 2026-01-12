dba883c58aad583f0b84e9d865f6d2e147dd99ca
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2026-01-12 15:15:05 +0530

    Re-add WiFi and HTTPClient includes in Api_post.ino

--- API/Api_post.ino
+++ API/Api_post.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h> // wifi se connect krne ke liye 
+#include <WiFi.h> // wifi se connect krne ke liye
 #include <HTTPClient.h> //rek or response handel krne ke liye 
 #include <ArduinoJson.h> // JSON data banane aur send karne ke liye
 
@@ -59,3 +59,4 @@ void loop() {
 
   delay(5000);
 }
+
